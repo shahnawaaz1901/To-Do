81fa05f2006fe12e46fb4569e3a82b83c011d7a7
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-07 20:55:28 +0530

    Added More Featrues to the App

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const todoContainer = document.querySelector("todos");
 const choices = document.querySelectorAll(".choices span");
 
 let currentChoice = choices[0];
-
+const deleteButtons = document.querySelectorAll("delete-btn");
 for (let choice of choices) {
   choice.addEventListener("click", function (e) {
     makSelectElementBold(this);
@@ -19,4 +19,14 @@ function makSelectElementBold(element) {
   currentChoice.classList.remove("bold");
   element.classList.add("bold");
   currentChoice = element;
+}
+
+// for(let every of deleteButtons){
+//     every.addEventListener("click",(id)=>{
+//         console.log(id)
+//     })
+// }
+
+function deleteTodo(id) {
+  console.log(id);
 }
