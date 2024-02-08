1d7be83fad3f5b557fbe0b2179d7073ad425b202
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-08 12:49:43 +0530

    Start to Implement more functionalities

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-console.log("Script is Connneted !!");
-
-const inputBox = document.querySelector("input-todo");
-const todoContainer = document.querySelector("todos");
+const inputBox = document.querySelector("#input-todo");
+const todoContainer = document.querySelector(".todos");
 const choices = document.querySelectorAll(".choices span");
+const addBtn = document.querySelector("#plus");
+const deleteButtons = document.querySelectorAll("delete-btn");
 
 let currentChoice = choices[0];
-const deleteButtons = document.querySelectorAll("delete-btn");
+
+const todos = [];
+const todoCount = todos.length;
+
 for (let choice of choices) {
   choice.addEventListener("click", function (e) {
-    makSelectElementBold(this);
+    makeSelectElementBold(this);
   });
 }
-document.addEventListener("DOMContentLoaded", (e) => {
-  makSelectElementBold(choices[0]);
-});
 
-function makSelectElementBold(element) {
+function makeSelectElementBold(element) {
   currentChoice.classList.remove("bold");
   element.classList.add("bold");
   currentChoice = element;
 }
 
-// for(let every of deleteButtons){
-//     every.addEventListener("click",(id)=>{
-//         console.log(id)
-//     })
-// }
-
 function deleteTodo(id) {
   console.log(id);
 }
+
+function renderTodo(todo) {
+  console.log(todo);
+}
+
+function addTodo(todo) {
+  todos.push(todo);
+  todoContainer.innerHTML = "";
+  for (let everyTodo of todos) {
+    renderTodo(everyTodo);
+  }
+}
+
+addBtn.addEventListener("click", (e) => {
+  if (inputBox.value) {
+    console.log(inputBox.value);
+    inputBox.value = "";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  makeSelectElementBold(choices[0]);
+});
