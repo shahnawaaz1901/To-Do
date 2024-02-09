aed8553e969c5cf768a414757221569aebef2a75
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2024-02-09 21:15:25 +0530

    Added More Feature to the App

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const todoContainer = document.querySelector(".todos");
 const choices = document.querySelectorAll(".choices span");
 const addBtn = document.querySelector("#plus");
 const deleteButtons = document.querySelectorAll("delete-btn");
-
+let numTodo = document.getElementById("task-count");
 let currentChoice = choices[0];
 
-const todos = [];
+const todos = [
+  {
+    completed: true,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    completed: true,
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+];
 const todoCount = todos.length;
 
 for (let choice of choices) {
@@ -26,15 +35,28 @@ function deleteTodo(id) {
 }
 
 function renderTodo(todo) {
-  console.log(todo);
+  const newElement = document.createElement("div");
+  newElement.classList.add("todo");
+  newElement.innerHTML = `<div class="select-btn">
+  <input type="radio" ${todo.completed ? "checked" : ""}/>
+</div>
+<div class="todo-task">
+  ${todo.text}
+</div>
+<div class="operation-btn">
+  <div class="delete-btn" onclick="deleteTodo(2)">
+    <i class="fa-solid fa-delete-left"></i>
+  </div>
+  <div class="update-btn" onclick="updateTodo(Nothing)">
+    <i class="fa-regular fa-pen-to-square"></i>
+  </div>
+</div>`;
+  todoContainer.appendChild(newElement);
 }
 
 function addTodo(todo) {
   todos.push(todo);
-  todoContainer.innerHTML = "";
-  for (let everyTodo of todos) {
-    renderTodo(everyTodo);
-  }
+  renderList();
 }
 
 addBtn.addEventListener("click", (e) => {
@@ -44,6 +66,14 @@ addBtn.addEventListener("click", (e) => {
   }
 });
 
+function renderList() {
+  todoContainer.innerHTML = "";
+  for (let everyTodo of todos) {
+    renderTodo(everyTodo);
+  }
+  numTodo.textContent = todos.length;
+}
 document.addEventListener("DOMContentLoaded", (e) => {
   makeSelectElementBold(choices[0]);
+  renderList();
 });
